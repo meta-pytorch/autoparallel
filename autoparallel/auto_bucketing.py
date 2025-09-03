68142a3d15353b814bc5da950df4110c693f7754
ruisizhang123 <ruisizhang123@gmail.com>
2025-09-03 13:34:12 -0700

    Update
    [ghstack-poisoned]

--- autoparallel/auto_bucketing.py
+++ autoparallel/auto_bucketing.py
@@ -1,5 +1,9 @@
-import torch
+# Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
+#
+# This source code is licensed under the BSD license found in the
+# LICENSE file in the root directory of this source tree.
 
+import torch
 from .autobucketing_util import bucket_utils
 
 
