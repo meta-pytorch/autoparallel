28431e901b4acc0813b46c6dd67f5ac48a255f9b
Simon Fan <xmfan@meta.com>
2025-10-13 08:54:46 -0700

    Support fx_traceback.annotate w/ local_map and add tests
    stack-info: PR: https://github.com/meta-pytorch/autoparallel/pull/203, branch: xmfan/stack/13

--- tests/test_api.py
+++ tests/test_api.py
@@ -5,6 +5,7 @@
 
 import pytest
 import torch
+import torch.fx.traceback as fx_traceback
 from torch import nn
 from torch.distributed.tensor.placement_types import Shard
 from torch.testing._internal.distributed.fake_pg import FakeStore
@@ -114,3 +115,83 @@ def test_init(device_mesh_1d):
     assert torch.equal(
         parallel_mod.get_buffer("buf").full_tensor(), torch.arange(dim, device="cuda")
     )
+
+
+def test_fx_graph_annotate(device_mesh_1d):
+    dim = 128
+
+    class Model(nn.Module):
+        def __init__(self, dim):
+            super().__init__()
+            self.a = nn.Linear(dim, dim, bias=False)
+            self.b = nn.Linear(dim, dim, bias=False)
+            self.c = nn.Linear(dim, dim, bias=False)
+            self.d = nn.Linear(dim, dim, bias=False)
+
+        def forward(self, x):
+            with fx_traceback.annotate({"outer": 0}):
+                with fx_traceback.annotate({"inner": 0}):
+                    a = self.a(x)
+                with fx_traceback.annotate({"inner": 1}):
+                    b = self.b(a)
+                with fx_traceback.annotate({"inner": 2}):
+                    c = self.c(b)
+                with fx_traceback.annotate({"inner": 3}):
+                    d = self.d(c)
+            return d
+
+    def input_fn():
+        b = 512
+        inputs = (torch.rand(b, dim, device="cuda"),)
+        return inputs
+
+    with torch.device("meta"):
+        model = Model(dim)
+
+    with fx_traceback.preserve_node_meta(), AutoParallel(
+        model,
+        input_fn,
+        device_mesh_1d,
+    ) as autop:
+        x_sharding = (Shard(0),)
+        autop.add_input_constraints([x_sharding])
+        sharding_placement = autop.optimize_placement()
+
+        # AutoParallel produces a module with meta-DTensor parameters that need to be initialized
+        _ = autop.apply_placement(sharding_placement)
+
+    graph = autop.parallel_gm.graph
+
+    # 4 linear -> 4 mm ops
+    fw_seen_annotations = set()
+    bw_seen_annotations = set()
+    for mm in [n for n in graph.nodes if "mm" in n.name]:
+        assert mm.meta["custom"]["outer"] == 0
+        assert "inner" in mm.meta["custom"]
+        if mm.meta.get("partitioner_tag", "") == "is_backward":
+            bw_seen_annotations.add(mm.meta["custom"]["inner"])
+        else:
+            fw_seen_annotations.add(mm.meta["custom"]["inner"])
+    assert fw_seen_annotations == bw_seen_annotations == {0, 1, 2, 3}
+
+    for ph in graph.find_nodes(op="placeholder"):
+        assert (
+            "custom" not in ph.meta
+        ), "Placeholders didn't have have custom metadata before"
+    for out in graph.find_nodes(op="output"):
+        assert (
+            "custom" not in out.meta
+        ), "Output didn't have have custom metadata before"
+
+    # NOTE: The tests below are just to prevent semantics from changing silently.
+    # Currently, custom metadata is not set for:
+    # - graph inputs
+    # - graph outputs
+    # - collectives/waits added by AP
+    for node in graph.nodes:
+        if node.meta.get("custom", None) is None:
+            assert (
+                node.op == "placeholder"
+                or node.op == "output"
+                or node.target.namespace == "_c10d_functional"
+            )
