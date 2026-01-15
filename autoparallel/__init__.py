33ef5996e7508dd5f299f4c23a1ba89d7b2b7f36
Francisco Massa <fvsmassa@gmail.com>
2026-01-15 14:11:23 +0000

    Export autoparallel.autobucketing for BC with torchtitan

--- autoparallel/__init__.py
+++ autoparallel/__init__.py
@@ -2,3 +2,6 @@
 #
 # This source code is licensed under the BSD license found in the
 # LICENSE file in the root directory of this source tree.
+
+# keep it for BC for now
+from .graph_passes import auto_bucketing  # noqa: F401
