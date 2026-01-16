a4d868ce966bcffde5098bd8b35b18d6fe254c0a
Francisco Massa <fvsmassa@gmail.com>
2026-01-16 10:46:44 +0000

    Keep BC with torchtitan for now

--- autoparallel/auto_bucketing.py
+++ autoparallel/auto_bucketing.py
@@ -2,3 +2,6 @@
 #
 # This source code is licensed under the BSD license found in the
 # LICENSE file in the root directory of this source tree.
+
+# Backward compatibility shim: re-export everything from the actual module location
+from autoparallel.graph_passes.auto_bucketing import *  # noqa: F401, F403
