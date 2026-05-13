8ec2642cfd5d977a921bdb62755cc77775debae9
Francisco Massa <fvsmassa@gmail.com>
2026-05-13 12:52:00 +0000

    Fix header

--- examples/example_save_llama3.py
+++ examples/example_save_llama3.py
@@ -1,3 +1,8 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
+#
+# This source code is licensed under the BSD license found in the
+# LICENSE file in the root directory of this source tree.
+
 # Example: Trace LLaMA-3 8B, optimize, and save the optimizer state.
 #
 # This script runs the expensive tracing + ILP construction once and saves
